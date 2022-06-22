a2479a7e4ba80158e577d909353ef6d1b62f2458
EighteenLao <e80677@yahoo.com.tw>
2022-06-22 11:11:28 +0800

    verify

--- connect.php
+++ connect.php
@@ -5,6 +5,7 @@
   $db_name = 'roommemberdatabase';
 
   // mysqli 的四個參數分別為：伺服器名稱、帳號、密碼、資料庫名稱
+  // mysqli: 永遠連線函式，mysqli多次執行mysqli將使用同一連線程序，從而減少了伺服器的開銷。
   $conn = new mysqli($server_name, $username, $password, $db_name);
 
   if (!empty($conn->connect_error)) {
--- verify.php
+++ verify.php
@@ -1,26 +1,33 @@
 <?php
+  session_start(); 
   // 連線到資料庫
+  // require_once適合用在:PHP程式執行期間，會多次引入相同的檔案。
+  // 確保不會因為重覆引入相同的檔案，而產生函數重覆定義以及重覆給值的錯誤。
   require_once('connect.php');
 
-  if(isset($_POST['login'])){
-    $user=$_POST["login"];
-    $password=$_POST["password"];
-    $flag = 0;
+  $user=$_POST["login"];
+  $password=$_POST["password"];
+  $reUser = preg_replace('/[\'\@\.\;\-=" "]+/', '', $user); //過濾特殊字元
+  $rePassword = preg_replace('/[\'\@\.\;\-=" "]+/', '', $password); //過濾特殊字元
 
-    $sql = "SELECT password FROM  loginadmin WHERE user = \"".$user."\";";
+  if($user && $password){
+    $sql = "SELECT password FROM  loginadmin WHERE user = \"".$reUser."\" and password = \"".$rePassword."\";";
     $result = mysqli_query($conn, $sql) or die('MySQL query error');
-
-    while ($row = $result->fetch_assoc()) {
-      if ($row['password'] == $password){
-        $flag = 1;
-        header(("Location: index.php"));
-      }
-    }
+    $row = $result->fetch_assoc();
     
-    if ($flag == 0){
-      header(("Location: login.html")); 
+    if($row){
+      $_SESSION['is_login'] = TRUE;
+      header(("Location: index.php"));
     }
-  
+    else{
+      $_SESSION['is_login'] = FALSE;
+      $_SESSION['msg'] = '登入失敗，請確認帳號密碼!!';
+      header(("Location: login.html"));
+    }
+  }
+  else{
+    $_SESSION['msg'] = '請輸入帳號密碼!!';
+    header(("Location: login.html"));
   }
 
 ?>
